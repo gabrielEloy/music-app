591a199a25b22b72038ca0d8c03bbac58456a0ed
Gabriel Eloy <gabrieleloy@pop-os.localdomain>
2021-02-09 20:53:12 -0300

    added mocked API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,22 @@
 import Head from "next/head";
+import * as React from 'react';
 import { AlbumCard } from "../components/AlbumCard";
 import styles from "../styles/Home.module.css";
 import { motion } from "framer-motion";
 import stagger from "../animations/stagger";
 import fadeInUp from "../animations/fadeInUp";
 
-const mockedCover = [
-  {
-    cover:
-      "https://portalfamosos.com.br/wp-content/uploads/2020/07/EKkYukOUUAEdCwR.png",
-    name: "when we all fall asleep where do we go",
-    artist: "Billie Eilish - 2019",
-  },
-  {
-    cover:
-      "http://loggado.com/wp-content/uploads/2017/06/Lorde-Melodrama-2.jpg",
-    name: "Melodrama",
-    artist: "Lorder - 2017",
-  },
-  {
-    cover: "https://images-na.ssl-images-amazon.com/images/I/71sHlq8VDQL.jpg",
-    name: "25",
-    artist: "Adelle - 2015",
-  },
-  {
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/I/71DX9fQhSHL._SL1212_.jpg",
-    name: "Beerbongs & bentleys",
-    artist: "Post Malone - 2018",
-  },
-  {
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/I/71xNOkPaOHL._AC_SX425_.jpg",
-    name: "If You're Reading This It's Too Late",
-    artist: "Drake - 2015",
-  },
-  {
-    cover:
-      "http://lojasaraiva.vteximg.com.br/arquivos/ids/2683306/1002918405.jpg?v=637020949757800000",
-    name: "Beauty Behind the Madness",
-    artist: "The Weeknd - 2015",
-  },
-  {
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/I/612luc2DEML._AC_SL1000_.jpg",
-    name: "Future Nostalgia",
-    artist: "Dua Lipe - 2019",
-  },
-];
+Home.getInitialProps = async function() {
+  const res = await fetch(
+    "http://my-json-server.typicode.com/gabrielEloy/music-app/musics"
+  );
+  const albums = await res.json();
+  return {
+    albums
+  };
+};
 
-export default function Home() {
+export default function Home({albums}) {
   return (
     <motion.div
       exit={{ opacity: 0 }}
@@ -68,30 +35,29 @@ export default function Home() {
         animate="animate"
         className={styles.main}
       >
-        <h1>Musics App</h1>
+        <motion.h1
+          className={styles.home__title}
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.6,
+            delay: 0.4,
+          }}
+        >
+          Musics App
+        </motion.h1>
         <motion.div variants={stagger} className={styles.albums}>
-          {mockedCover.map(({ cover, name, artist }) => (
-            <motion.div
-              className="album"
-              key={cover}
-              variants={fadeInUp()}
-            >
+          {albums.map(({ cover, name, artist }) => (
+            <motion.div className="album" key={cover} variants={fadeInUp()}>
               <AlbumCard name={name} artist={artist} cover={cover} />
             </motion.div>
           ))}
         </motion.div>
       </motion.main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <motion.img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </motion.div>
   );
 }
